0979052892732e558a8f3fa6da29c836215b0fa5
Alex Guadalupe <9035@holbertonstudents.com>
2024-04-16 18:59:50 -0400

    add main.h

---
+++ main.h
@@ -0,0 +1,14 @@
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+int main (void);
+int main(int argc, char *argv[]);
+int main(int argc, char argv[], charenvp[]);
+int main(int argc, char  **argv);
+
+#endif
