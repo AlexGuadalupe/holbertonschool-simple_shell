4be444dd3b35fdd7af3bc7821e8e9ba0cb196aed
Alex Guadalupe <9035@holbertonstudents.com>
2024-04-17 16:28:58 -0400

    add info

--- hsh.c
+++ hsh.c
@@ -1,42 +1,41 @@
 #include "shell.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <sys/wait.h>
-#include <string.h>
-
 #define MAX_COMMAND_LENGTH 1024
 
 int main() {
-    char command[MAX_COMMAND_LENGTH];
-    pid_t pid;
+char command[MAX_COMMAND_LENGTH];
+pid_t pid;
 
-    while (1) {
-        printf("$ ");
-        fflush(stdout);
+while (1)
+{
+printf("$");
+fflush(stdout);
 
-        if (fgets(command, MAX_COMMAND_LENGTH, stdin) == NULL) {
-            printf("\n");
-            break;
-        }
+if (fgets(command, MAX_COMMAND_LENGTH, stdin) == NULL)
+{
+printf("\n");
+break;
+}
 
-        command[strcspn(command, "\n")] = '\0';
+command[strcspn(command, "\n")] = '\0';
 
-        if (strcmp(command, "exit") == 0) {
-            break;
-        }
+if (strcmp(command, "exit") == 0)
+{
+break;
+}
 
-        pid = fork();
-        if (pid == 0) {
-            if (execlp(command, command, NULL) == -1) {
-                perror("Error al ejecutar el comando");
-                exit(EXIT_FAILURE);
-            }
-        } else if (pid < 0) {
-            perror("Error al crear el proceso hijo");
-        } else {
-            wait(NULL);
-        }
-    }
-    return 0;
+pid = fork();
+if (pid == 0) {
+if (execlp(command, command, NULL) == -1)
+{
+perror("Error al ejecutar el comando");
+exit(EXIT_FAILURE);
+}
+} else if (pid < 0)
+{
+perror("Error al crear el proceso hijo");
+} else {
+wait(NULL);
+}
+}
+return 0;
 }
