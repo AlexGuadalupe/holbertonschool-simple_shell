a5e7df14684f0fd556c206ab98d3f6a9cfcd4f44
Rogergrosado <rgrosado1@gmail.com>
2024-04-18 21:36:36 -0400

    making validators.c

--- validators.c
+++ validators.c
@@ -1,21 +1,16 @@
 #include "main.h"
 
-/**
- * validate_input - Validate if PATH exists
- * @arguments: String of tokens
- * @argv: String of arguments from input
- * Return: A pointer to an array of the tokenized PATH directories
- **/
+
 char *validate_input(char **arguments, char **argv __attribute__((unused)))
 {
 	char *new_arguments, *first, *slash_argument = "/";
 	char **tokens_path, holder_env[1024];
 	int i = 0;
 
-	if (arguments[0][0] == '/' || arguments[0][0] == '.') /*check first element */
+	if (arguments[0][0] == '/' || arguments[0][0] == '.') 
 	{
-		new_arguments = arguments[0]; /* point to the same address */
-		if ((access(new_arguments, F_OK) == -1)) /* check if file exits */
+		new_arguments = arguments[0];
+		if ((access(new_arguments, F_OK) == -1)) 
 		{
 			fprintf(stderr, "%s: No such file or directory\n", arguments[0]);
 			return ("Fail access");
@@ -23,11 +18,11 @@ char *validate_input(char **arguments, char **argv __attribute__((unused)))
 	}
 	else
 	{
-		strcpy(holder_env, getenv("PATH"));/*get PATH and copy it to holder_env buf*/
-		tokens_path = tokenizer_path(holder_env);/*tokenize PATH environment string*/
+		strcpy(holder_env, getenv("PATH"));
+		tokens_path = tokenizer_path(holder_env);
 		while (tokens_path[i])
 		{
-			/* check if file exists */
+			
 			first = str_concat(slash_argument, arguments[0]);
 			new_arguments = str_concat(tokens_path[i], first);
 			if ((access(new_arguments, F_OK) == -1))
@@ -50,11 +45,7 @@ char *validate_input(char **arguments, char **argv __attribute__((unused)))
 	return (new_arguments);
 }
 
-/**
- * validate_spaces - Validate spaces, tabs and line breaks
- * @input: String from input
- * Return: 0 if not find spaces, tabs or line break
- */
+
 int validate_spaces(char *input)
 {
 	int i = 0, flag = 1, len = 0;
