eefb0ab3d23afa88c925b2f95743831d29edeec6
Rogergrosado <rgrosado1@gmail.com>
2024-04-22 15:37:17 -0400

    adding comments to validators.c

--- validators.c
+++ validators.c
@@ -1,6 +1,22 @@
-#include "main.h"
+#include "shell.h"
 
-
+/**
+*   Function: validate_input
+*   Parameters: char **arguments - array of arguments, char **argv - array of program arguments (unused)
+*   Return: char * - validated input or error message
+*   
+*   This function validates the input arguments and checks if the specified file exists in the system.
+*
+*   Check if the argument specifies an absolute or relative path
+*   Check if the file exists
+*   Get the PATH environment variable
+*   Tokenize the PATH variable
+*   Iterate through each directory in PATH
+*   Concatenate "/" with the argument to form a path
+*   Check if the file exists in the current directory
+*   Free memory and return the validated input
+*   Free memory and print error message if file not found
+*/
 char *validate_input(char **arguments, char **argv __attribute__((unused)))
 {
 	char *new_arguments, *first, *slash_argument = "/";
@@ -45,7 +61,15 @@ char *validate_input(char **arguments, char **argv __attribute__((unused)))
 	return (new_arguments);
 }
 
-
+/* 
+*   Function: validate_spaces
+*   Parameters: char *input - input string
+*   Return: int - flag indicating presence of spaces
+*   
+*   This function validates if the input string contains only spaces, tabs, or newlines.
+*   Skip leading spaces, tabs, and newlines
+*   If the string is not entirely spaces, tabs, or newlines, set flag to 0
+*/
 int validate_spaces(char *input)
 {
 	int i = 0, flag = 1, len = 0;
