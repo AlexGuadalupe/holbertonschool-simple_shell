418b9e14814de19207c83b2616e80ea97a6a4f11
Your Name <saul.vera787@gmail.com>
2024-04-24 20:17:56 -0400

    handling output errors

--- hsh.c
+++ hsh.c
@@ -1,31 +1,47 @@
 #include "shell.h"
+#include <ctype.h>
 #define MAX_COMMAND_LENGTH 1024
 
 int main() {
 char command[MAX_COMMAND_LENGTH];
 pid_t pid;
+char *ptr;
 
 while (1)
 {
 printf("$");
-fflush(stdout);
 
 if (fgets(command, MAX_COMMAND_LENGTH, stdin) == NULL)
 {
 printf("\n");
-break;
+exit(0);
 }
 
 command[strcspn(command, "\n")] = '\0';
 
-if (strcmp(command, "exit") == 0)
+ptr = command + strlen(command) - 1;
+while(ptr > command && isspace(*ptr))
+    --ptr;
+system(command);
+ptr[1] = '\0';
 {
-break;
 }
-
+if (strcmp(command, "ls -l") == 0)
+{
+  system("ls -l");
+}
+else if (strcmp(command, "ls") == 0)
+{
+  system("ls");
+}
+else if (strcmp(command, "exit") == 0)
+{
+exit(0);
+}
+}
 pid = fork();
 if (pid == 0) {
-if (execlp(command, command, NULL) == -1)
+if (system(command) == -1)
 {
 perror("Error al ejecutar el comando");
 exit(EXIT_FAILURE);
@@ -35,7 +51,6 @@ exit(EXIT_FAILURE);
 perror("Error al crear el proceso hijo");
 } else {
 wait(NULL);
-}
 }
 return 0;
 }
