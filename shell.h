b0af4e210f9b1569a55b34aca4f4edc3766e4b98
Alex Guadalupe <9035@holbertonstudents.com>
2024-04-24 22:54:54 -0400

    edit file shell.h

--- shell.h
+++ shell.h
@@ -14,13 +14,13 @@ extern char **environ;
 #define DELIM " \t\r\n\a"
 #define PROMPT "simple_shell "
 
-int _printenv(void);
-int cmd_read(char *s, size_t __attribute__((unused)) file_stream, char *name);
-char *pathfinder(char *cmd);
+void print_env(void);
+int read_command(char *input, size_t __attribute__((unused)) file_stream, char *name);
+char *search_path(char*command);
 void print_not_found(char *cmd, char *name);
-int call_command(char *cmd_arr[], char *name);
-int _str_n_cmp(char *s1, char *s2, int n);
-char *_getenv(char *var);
+int read_command(char *cmd_arr[], char *name);
+int _str_cmp(char *string1, const char *string2, int num_chars);
+char *_getenv(const char *env_var);
 int execute_command(char *cmd_arr[], char *name);
 
 #endif /* SHELL_H */
